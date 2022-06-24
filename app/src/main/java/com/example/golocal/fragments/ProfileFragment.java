5c332c0c71b78795627f04f733b127a765fd5bef
Emily Thompson <emilymthompson@fb.com>
2022-06-24 14:48:42 -0700

    user can take a picture to update profile image

--- app/src/main/java/com/example/golocal/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/golocal/fragments/ProfileFragment.java
@@ -1,8 +1,15 @@
 package com.example.golocal.fragments;
 
+import static android.app.Activity.RESULT_OK;
+
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -12,20 +19,26 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.Target;
 import com.example.golocal.R;
 import com.example.golocal.activities.LoginActivity;
 import com.parse.ParseException;
 import com.parse.ParseFile;
 import com.parse.ParseUser;
 import com.parse.SaveCallback;
+
+import java.io.File;
 
 public class ProfileFragment extends Fragment {
     private TextView tvUsername;
@@ -34,11 +47,15 @@ public class ProfileFragment extends Fragment {
     private TextView tvEditBio;
     private EditText etBio;
     private Button btFinishEditBio;
+    private ImageButton ibEditProfileImage;
     private ParseUser user;
     private final String KEY_BIO = "bio";
     private final String KEY_IMAGE = "profileImage";
     private Context context;
     private final String TAG = "ProfileFragment";
+    public static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 42;
+    private File photoFile;
+    public String photoFileName = "photo.jpg";
 
     public ProfileFragment(ParseUser currentUser) {
         user = currentUser;
@@ -88,6 +105,7 @@ public class ProfileFragment extends Fragment {
         tvEditBio = view.findViewById(R.id.tvEditBio);
         etBio = view.findViewById(R.id.etBio);
         btFinishEditBio = view.findViewById(R.id.btFinishEditBio);
+        ibEditProfileImage = view.findViewById(R.id.ibEditProfileImage);
 
         tvUsername.setText(user.getString("username"));
         tvBio.setText(user.getString("bio"));
@@ -95,6 +113,14 @@ public class ProfileFragment extends Fragment {
         if (image != null) {
             Glide.with(this).load(image.getUrl()).circleCrop().into(ivProfileImage);
         }
+
+        ibEditProfileImage.setImageDrawable(getContext().getDrawable(R.drawable.add_button_outline_24));
+        ibEditProfileImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchCamera();
+            }
+        });
 
         tvEditBio.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -126,5 +152,56 @@ public class ProfileFragment extends Fragment {
             }
         });
 
+    }
+
+    private void launchCamera() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        photoFile = getPhotoFileUri(photoFileName);
+
+        Uri fileProvider = FileProvider.getUriForFile(getContext(), "com.codepath.fileprovider", photoFile);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
+
+        if (intent.resolveActivity(context.getPackageManager()) != null) {
+            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
+        }
+    }
+
+    public File getPhotoFileUri(String fileName) {
+        File mediaStorageDir = new File(getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES), TAG);
+
+        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()) {
+            Log.d(TAG, "failed to create directory");
+        }
+        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
+        return file;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                // by this point we have the camera photo on disk
+                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
+
+                Bitmap resizedBitmap = Bitmap.createScaledBitmap(takenImage, 100, 100, true);
+                ivProfileImage.setImageBitmap(resizedBitmap);
+                user.put(KEY_IMAGE, new ParseFile(photoFile));
+                user.saveInBackground(new SaveCallback() {
+                    @Override
+                    public void done(ParseException e) {
+                        if (e != null) {
+                            Log.e(TAG, "Error while saving profile image", e);
+                            Toast.makeText(context, "Error while saving profile image", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+                Glide.with(this)
+                        .load(user.getParseFile(KEY_IMAGE).getUrl())
+                        .override(Target.SIZE_ORIGINAL, Target.SIZE_ORIGINAL)
+                        .circleCrop()
+                        .into(ivProfileImage);
+            } else {
+                Toast.makeText(getContext(), "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
